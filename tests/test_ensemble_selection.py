534c4f5e2d9822473a4a1cb076fb34bfa7efc5ec
Sebastian Pölsterl <sebp@k-d-w.org>
2019-02-17 18:45:20 +0100

    Reduce alpha values for survival SVM to improve numerical stability

--- tests/test_ensemble_selection.py
+++ tests/test_ensemble_selection.py
@@ -222,7 +222,7 @@ class DummySurvivalRegressor(DummyRegressor):
 
 def _create_regression_ensemble():
     aft_grid = ParameterGrid({"alpha": 2. ** numpy.arange(-9, 5, 2)})
-    svm_grid = ParameterGrid({"alpha": 2. ** numpy.arange(-9, 5, 2)})
+    svm_grid = ParameterGrid({"alpha": 2. ** numpy.arange(-12, 0, 2)})
 
     base_estimators = []
     for i, params in enumerate(aft_grid):
@@ -251,11 +251,11 @@ class TestEnsembleSelectionRegressor(object):
 
         meta.fit(whas500.x[:400], whas500.y[:400])
         assert len(meta) == 5
-        assert meta.scores_.shape == (14,)
+        assert meta.scores_.shape == (13,)
 
         p = meta.predict(whas500.x[400:])
         score = numpy.sqrt(mean_squared_error(whas500.y[400:]['lenfol'], p))
-        assert abs(score - 1500.01954367) <= 0.1
+        assert abs(score - 1343.7278664531248) <= 0.1
 
     @staticmethod
     def test_fit_dummy(make_whas500):
